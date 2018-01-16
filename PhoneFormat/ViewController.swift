52412fed13463cf946f93789a86939622b127fe3
Kostyantyn Runduyev <runduev@gmail.com>
2018-01-17 00:32:21 +0300

    update

--- PhoneFormat/ViewController.swift
+++ PhoneFormat/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import PhoneNumberKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        let phoneNumberKit = PhoneNumberKit()
+//
+//        do {
+//            let phoneNumber = try phoneNumberKit.parse("+33 6 89 017383")
+//            let phoneNumberCustomDefaultRegion = try phoneNumberKit.parse("+44 20 7031 3000", withRegion: "GB", ignoreType: true)
+//
+//            let rawNumberArray = ["0291 12345678", "+49 291 12345678", "04134 1234", "09123 12345"]
+//            let phoneNumbers = phoneNumberKit.parse(rawNumberArray)
+//            let phoneNumbersCustomDefaultRegion = phoneNumberKit.parse(rawNumberArray, withRegion: "DE",  ignoreType: true)
+//
+//            print(phoneNumbers)
+//
+//            let a = phoneNumberKit.format(phoneNumber, toType: .e164) // +61236618300
+//            let b = phoneNumberKit.format(phoneNumber, toType: .international) // +61 2 3661 8300
+//            let c = phoneNumberKit.format(phoneNumber, toType: .national) // (02) 3661 8300
+//
+//            let d = phoneNumberKit.countries(withCode: 33)
+//            let e = phoneNumberKit.countryCode(for: "FR")
+//        }
+//        catch {
+//            print("Generic parser error")
+//        }
+        
+        
     }
-
 
 }
 
